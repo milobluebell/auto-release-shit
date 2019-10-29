ba75dc53154b06d583388d289ceb9f56eec38c5a
milo ma <milobluebell@gmail.com>
2019-10-29 16:15:50 +0800

    feat: 现在设置保存成功会提示

--- dist/option.js
+++ dist/option.js
@@ -10,7 +10,13 @@ document.getElementById('submitBtn').onclick = function () {
   wrappers.forEach(item => {
     targetObj[item.children[0].id] = document.getElementById(item.children[0].id).value;
   })
-  chrome.storage.sync.set(targetObj);
+  chrome.storage.sync.set(targetObj, function (){
+    const theToast = document.getElementById('set-success-reminder');
+    theToast.style.display = 'block';
+    setTimeout(()=> {
+      theToast.style.display = 'none';
+    }, 1600)
+  });
 }
 
 chrome.storage.sync.get(ids, function (res) {
