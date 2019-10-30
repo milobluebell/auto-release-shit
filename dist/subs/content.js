ba2e75303869870eb8b5d0e6f03318ffa1c3de61
milo ma <milobluebell@gmail.com>
2019-10-30 17:35:37 +0800

    feat: 加入了格式化desc的功能

--- dist/subs/content.js
+++ dist/subs/content.js
@@ -70,20 +70,19 @@ function insertElemNodes(shit, needReminder) {
     impressionHtml = document.createElement('div');
     let impressionHtmlTemplate = ``;
     let impressionStyle = ``;
-    const marginTop = '173px';
+    const marginTop = '153px';
     const marginLeft = '18px';
     if (Object.prototype.toString.call(shit).toLowerCase() === '[object array]' && shit.length > 0) {
-      impressionHtmlTemplate = `<table class="shit" cellspacing="6"><tbody class="tobsTable-body">
-        <thead><tr><td colspan="2">【发版申请】</td></tr></thead>`;
+      impressionHtmlTemplate = `<table class="shit" cellspacing="6"><tbody class="tobsTable-body"><thead><tr><td colspan="2">【发版申请】</td></tr></thead>`;
       shit.forEach(item => {
-        impressionHtmlTemplate += `<tr class="shit-job">
+        impressionHtmlTemplate += `<tr class="shit-job" style="${item.key === '发版说明' ? 'position: relative' : ''}">
             <td class="key stage-cell">${item.key}： </td>
-            <td class="value stage-cell">${item.value === 'staging' ? 'Production' : item.value}</td>
+            <td class="value stage-cell" id="${item.key}">${item.value === 'staging' ? 'Production' : item.value}${(item.key === '发版说明' && item.value.length >= 70) ? '<button title="格式化" class="toggle-mode" onclick="const desc = document.getElementById(\'发版说明\').childNodes[0].nodeValue;document.getElementById(\'发版说明\').innerHTML = \'<li>--  \' + desc.split(\'\、\').join(\'<li>--  \')"><img src="/static/44b87a8b/images/48x48/notepad.png"/></button>' : ''}</td>
           </tr>`;
       });
       impressionHtml.innerHTML = impressionHtmlTemplate + '</tbody></table>';
       impressionStyle = document.createElement('style');
-      impressionStyle.innerHTML = `.shit{max-width:460px;width:100%;margin-left:${marginLeft};margin-top:${marginTop};border:solid 1px #ccc}.shit .key{font-size:12px;min-width:90px}.shit .value{font-size:14px;font-weight:bold;text-indent:3px}.shit tr:nth-of-type(3){background:rgba(0, 0, 0, .05)}`;
+      impressionStyle.innerHTML = `.shit{position:relative;max-width:460px;width:100%;margin-left:${marginLeft};margin-top:${marginTop};border:solid 1px #ccc}.shit li{list-style:none}.toggle-mode{cursor:pointer;position:absolute;bottom:3px;right:3px;padding:0;margin:0;text-align:center;line-height:12px}.toggle-mode img{width:13px;height:13px;}.shit .key{font-size:12px;min-width:90px}.shit .value{font-size:14px;font-weight:bold;text-indent:3px}.shit tr:nth-of-type(3){background:rgba(0, 0, 0, .05)}`;
     } else {
       impressionHtmlTemplate = `<div class="shit">⚠️没有找到最后一次构建的git commit messages</div>`;
       impressionHtml.innerHTML = impressionHtmlTemplate;
@@ -92,7 +91,7 @@ function insertElemNodes(shit, needReminder) {
     }
     // 加提示
     if (needReminder) {
-      impressionHtml.innerHTML += `<span class="reminder">⚠️建议在"选项"中配置常用字段 -- Auto Release Sh*t</span><style>.reminder{margin-left:${marginLeft};font-size:12px;color:#b3b3b3}</style>`;
+      impressionHtml.innerHTML += `<span class="reminder">⚠️建议在"选项"中补全常用字段 -- Auto Release Sh*t</span><style>.reminder{margin-left:${marginLeft};font-size:12px;color:#b3b3b3}</style>`;
     }
     //
     targetDiv.appendChild(impressionHtml);
@@ -101,6 +100,13 @@ function insertElemNodes(shit, needReminder) {
   catch (err_msg) {
     console.warn(err_msg);
   }
+}
+
+/**
+ * 
+ */
+function toggleDescMode(desc) {
+  console.log(document.getElementById('发版说明'));
 }
 
 /**
