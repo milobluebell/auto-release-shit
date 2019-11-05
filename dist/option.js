c445eead934d979c08200e7ca843a2c13788785b
milo ma <milobluebell@gmail.com>
2019-11-05 20:15:06 +0800

    fix: 当没有保存设置内容时给予正确的提示

--- dist/option.js
+++ dist/option.js
@@ -9,13 +9,14 @@ document.getElementById('submitBtn').onclick = function () {
   const targetObj = {};
   wrappers.forEach(item => {
     targetObj[item.children[0].id] = document.getElementById(item.children[0].id).value;
-  })
+  });
   chrome.storage.sync.set(targetObj, function () {
     const theToast = document.getElementById('set-success-reminder');
+    theToast.innerHTML = Object.values(targetObj).every(item => item) > 0 ? '保存成功' : '没有填写内容';
     theToast.style.display = 'block';
     setTimeout(() => {
       theToast.style.display = 'none';
-    }, 1600)
+    }, 888)
   });
 }
 
