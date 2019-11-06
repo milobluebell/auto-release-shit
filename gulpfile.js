582f71700822d1b4f29a683cd2b0e736c42069ac
milo ma <milobluebell@gmail.com>
2019-11-06 18:18:24 +0800

    refactor: 重构结构和代码

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const gulp = require("gulp");
 var crx = require('gulp-crx-pack');
 var manifest = require('./extension-src/manifest.json');
 
-gulp.task('pack', function() {
+gulp.task('pack', function () {
   return gulp.src('./extension-src')
     .pipe(crx({
       privateKey: fs.readFileSync('./dist.pem', 'utf8'),
-      filename: `${manifest.name}.crx`,
-      codebase: 'https://github.com/milobluebell/ARS-publishment/blob/master/dist.crx?raw=true',
+      filename: `Auto_Release_Shit.crx`,
+      codebase: 'https://github.com/milobluebell/ARS-publishment/blob/master/Auto_Release_Shit.crx?raw=true',
       updateXmlFilename: 'updates.xml'
     }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('./dist'));
 });
