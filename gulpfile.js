b296ed79aaba2c5c966f17b73bd4a774e0ad2b28
milo ma <milobluebell@gmail.com>
2019-11-07 15:46:04 +0800

    fix: 修改gulpfile.js进行测试

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,34 @@ const fs = require("fs");
 const gulp = require("gulp");
 const clean = require("gulp-clean");
 const crx = require('gulp-crx-pack');
+const http = require('https');
+const arg = require("minimist")(process.argv.slice(2));
+
+
+async function createRelease() {
+
+}
+
+async function getTags() {
+  return await http.request({
+    json: true,
+    headers: {
+      "content-type": "application/json",
+    },
+    method: 'get',
+    url: `https://api.github.com/repos/milobluebell/auto-release-shit/tags`
+  }, function (error, res, body) {
+    console.log(error);
+    console.log(res);
+    if (!error && res && res.statusCode === 200) {
+      return {
+        data: res,
+        latest: res[res.length - 1]
+      }
+    } else return undefined;
+  })
+}
+
 
 gulp.task('clean-scripts', function () {
   return gulp.src('./dist', { read: false, allowEmpty: true })
@@ -10,11 +38,13 @@ gulp.task('clean-scripts', function () {
 });
 
 gulp.task('pack', gulp.series(['clean-scripts'], function () {
+  getTags();
   return gulp.src('./extension-src')
     .pipe(crx({
-      privateKey: fs.readFileSync('./dist.pem', 'utf8'),
+      // privateKey: fs.readFileSync('./extension-src.pem', 'utf8'),
+      privateKey: arg.pem,
       filename: `Auto_Release_Shit.crx`,
-      codebase: 'https://github.com/milobluebell/ARS-publishment/blob/master/Auto_Release_Shit.crx?raw=true',
+      codebase: 'https://github.com/milobluebell/auto-release-shit/blob/master/Auto_Release_Shit.crx?raw=true',
       updateXmlFilename: 'updates.xml'
     }))
     .pipe(gulp.dest('./dist'));
